3b08b301719e23e782c06b312fce29bd6351728e
Dan-Hen <leo.clot16@gmail.com>
2020-12-30 23:24:33 +0100

    finish animation on homepage and position menu

--- src/main.js
+++ src/main.js
@@ -1,13 +1,51 @@
 import gsap from 'gsap'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import { ScrollTrigger, TimelineLite } from 'gsap/all'
 import SplitText from './js/SplitText'
 
 gsap.registerPlugin(ScrollTrigger)
 
+const mainColor = 'white'
+
 function Home () {
-  let typescript = new SplitText('.subtitle', { type: 'chars' }),
-    chars = typescript.chars
-  gsap.fromTo(chars, { opacity: 0 }, { opacity: 1, stagger: 0.03, duration: 0.03 })
+  const MainTypeScript = new SplitText('.subtitle', { type: 'chars' })
+
+  const tl = new TimelineLite()
+  tl
+    .fromTo('.title', { opacity: 0 }, { delay: 0.4, opacity: 1, duration: 2 }, 0)
+    .fromTo(MainTypeScript.chars, { backgroundColor: mainColor }, {
+      backgroundColor: 'transparent',
+      delay: 1,
+      stagger: 0.03,
+      duration: 0.03
+    }, 1)
+    .fromTo(MainTypeScript.chars, { opacity: 0 }, {
+      delay: 0.97,
+      opacity: 1,
+      stagger: 0.03,
+      duration: 0.03
+    }, 1)
+
+  let OptionTypeScript = new SplitText('.text', { type: 'chars' })
+  let sub = new TimelineLite({
+    onComplete: onComplete
+  })
+  sub
+    .fromTo(OptionTypeScript.chars, { backgroundColor: mainColor }, {
+      backgroundColor: 'transparent',
+      delay: 0.03,
+      stagger: 0.03,
+      duration: 0.03
+    }, 1)
+    .fromTo(OptionTypeScript.chars, { opacity: 0 }, {
+      delay: 0,
+      opacity: 1,
+      stagger: 0.03,
+      duration: 0.03
+    }, 1)
+
+  function onComplete () {
+
+  }
 }
 
 Home()

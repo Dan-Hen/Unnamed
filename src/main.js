a1cd486053baba253f250e87607d252ae2da2b9f
Dan-Hen <leo.clot16@gmail.com>
2020-12-30 18:41:19 +0100

    typewritter animation

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
-console.log('Hello World')
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import SplitText from './js/SplitText'
+
+gsap.registerPlugin(ScrollTrigger)
+
+function Home () {
+  let typescript = new SplitText('.subtitle', { type: 'chars' }),
+    chars = typescript.chars
+  gsap.fromTo(chars, { opacity: 0 }, { opacity: 1, stagger: 0.03, duration: 0.03 })
+}
+
+Home()
